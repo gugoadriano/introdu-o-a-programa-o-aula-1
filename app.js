0bf1db6ebeae50996fec45aa13b8a953edcb89fd
EI 6236 <ei6236@edu.fb.org.br>
2025-04-23 21:18:13 -0300

    mudança

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 
-        let nome ='Adriano';
-        console.log(nome);
+    let nome ='Adriano';
+    console.log(nome);
     console.log('resultado 1',(4==4)&&(10<5));
     console.log((7!=7)&&(5<4));
     console.log((10 > 8)&&(9!=7));
     console.log((17>=16)&&(14<=15));
+  
